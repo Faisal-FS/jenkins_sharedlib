73fcccf9c96c6170f8416cb26afa8d4f63fb19ac
Fahd Arshad <fahdarshad123@gmail.com>
2017-03-15 14:09:36 +0500

    Update JenkinsLibrary.groovy

--- src/utils/JenkinsLibrary.groovy
+++ src/utils/JenkinsLibrary.groovy
@@ -4,8 +4,8 @@ import java.nio.charset.StandardCharsets
 // Importing external jar file with the help of grapes
 @Grab(group='org.yaml', module='snakeyaml', version='1.17')
 import org.yaml.snakeyaml.*
-    
-import groovy.json.*
+
+import groovy.json.JsonBuilder
 
 def sample_return(String statement)
 {
