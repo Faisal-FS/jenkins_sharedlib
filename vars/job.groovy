e83d8dba63bdc69bba022291373906202d969f23
Fahd Arshad <fahdarshad123@gmail.com>
2017-03-15 13:34:36 +0500

    Update job.groovy

--- vars/job.groovy
+++ vars/job.groovy
@@ -19,7 +19,7 @@ def call(body) {
         git credentialsId: 'jenkins', url: 'ssh://git@code.xgrid.co:29418/source/ats.git'
     }
     
-    stage ('bootstrap')
+    stage ('Bootstrap')
     {
       echo "bootstrap stage"
     }
