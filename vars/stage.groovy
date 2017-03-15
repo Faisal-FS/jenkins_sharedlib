3cf0fab2b802f673b439efbeb7b44991a09ba1cc
Fahd Arshad <fahdarshad123@gmail.com>
2017-03-15 13:57:46 +0500

    Update stage.groovy

--- vars/stage.groovy
+++ vars/stage.groovy
@@ -1,1 +1,15 @@
 
+#!groovy
+
+def call(body) {
+
+  def args = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = args
+  body()
+
+ node('ats_slaves')
+  {
+    echo "$args" 
+  }
+}
