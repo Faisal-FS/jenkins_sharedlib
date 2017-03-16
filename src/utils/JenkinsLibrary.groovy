771ed76f184fa47834a16bd2a909f853f0a68936
Fahd Arshad <fahdarshad123@gmail.com>
2017-03-16 13:59:21 +0500

    Update JenkinsLibrary.groovy

--- src/utils/JenkinsLibrary.groovy
+++ src/utils/JenkinsLibrary.groovy
@@ -2,35 +2,43 @@ package utils;
 
 import java.nio.charset.StandardCharsets
 
-@NonCPS
-def create_stages(def args)
-{   
+@NonCPS def countStages(def args)
+{
    def list = []
-    def count = 0 
-    
-    for(emp in args) {
-      
+   def count = 0  
+   for(emp in args) {
       if (emp.toString().contains('stage'))
         {
           list.add("$emp")
           count = list.size()
-          //echo("$count")
         }
     }
+   
+   for (int a = 0; a < count ; a++)
+   {
+      create_stages(a,list[a])  
+   }
+}
+
+@NonCPS
+def create_stages(def number, def value)
+{   
+   echo ("$number")
+   echo ("$value")
     
-    String s1 = ""
-    for (def n in list) {
-        echo ("$n")
-        def (stage_name, command) = n.split('=')
+    //String s1 = ""
+    //for (def n in list) {
+      //  echo ("$n")
+       // def (stage_name, command) = n.split('=')
        
        //s1 = "stage ('testing') { echo \"test\" } "
        
        //return s1
-         stage("$stage_name")
-         {
-          echo ("$command")
-         }
-    }
+      //   stage("$stage_name")
+       //  {
+        //  echo ("$command")
+        // }
+    //}
     
 }
 return this;
