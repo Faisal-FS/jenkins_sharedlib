aa55944c5f7258feb97a29805402f92b10b8aa6e
Fahd Arshad <fahdarshad123@gmail.com>
2017-03-16 12:28:42 +0500

    Update JenkinsLibrary.groovy

--- src/utils/JenkinsLibrary.groovy
+++ src/utils/JenkinsLibrary.groovy
@@ -1,27 +1,6 @@
 package utils;
 
 import java.nio.charset.StandardCharsets
-// Importing external jar file with the help of grapes
-@Grab(group='org.yaml', module='snakeyaml', version='1.17')
-import org.yaml.snakeyaml.*
-
-@NonCPS
-def countStages(def args)
-{
-    def list = []
-    def count = 0 
-    
-    for(emp in args) {
-      
-      if (emp.toString().contains('stage'))
-        {
-          list.add("$emp")
-          count = list.size()
-          echo("$count")
-        }
-    }
-   return list; 
-}
 
 @NonCPS
 def create_stages(def list)
