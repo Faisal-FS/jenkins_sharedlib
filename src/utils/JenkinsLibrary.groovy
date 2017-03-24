e67a398efaf71355690568a6fc8dc0ed1d45dfb8
Fahd Arshad <fahdarshad123@gmail.com>
2017-03-24 18:21:00 +0500

    Update JenkinsLibrary.groovy

--- src/utils/JenkinsLibrary.groovy
+++ src/utils/JenkinsLibrary.groovy
@@ -38,6 +38,7 @@ def create_stages(def number, def valueString)
    def (key, stage_name) = fullstage.split('_') 
    stage ("$stage_name")
    {
+      echo ("$command");
       sh "echo $command; $command"
    } 
 }
