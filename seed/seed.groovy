0368cff1f2fe58c4c9ac1e8a7c5c060449bec33c
Fahd Arshad <fahdarshad123@gmail.com>
2017-04-13 13:42:50 +0500

    Update seed.groovy

--- seed/seed.groovy
+++ seed/seed.groovy
@@ -2,7 +2,7 @@
 
 //def test = build.getEnvironment(listener).get('test')
 //def env = System.getenv()
-println GIT_URL
+println TESING
 
 def alfred_list = readFileFromWorkspace('alfred_enabled.list')
 String[] split_file = alfred_list.split(System.getProperty("line.separator"));
