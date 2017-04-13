7b37c499cd36ef48fbc38419ed0f16018333cba3
Fahd Arshad <fahdarshad123@gmail.com>
2017-04-13 13:59:30 +0500

    Update seedjob.groovy

--- seed/seedjob.groovy
+++ seed/seedjob.groovy
@@ -3,5 +3,11 @@
 // Importing Config Slurper using grapes
 @Grab(group='org.apache.commons', module='commons-io', version='1.3.2')
 
+def sout = new StringBuilder(), serr= new StringBuilder()
 
-//println PROJECT
+// Clone Project, according to Gerrit Trigger
+def projectRoot = WORKSPACE + "/$PROJECT/"
+def clone = "git clone $PROJECTURL".execute(null, new File(WORKSPACE + "/"))
+clone.consumeProcessOutput(sout, serr)
+clone.waitFor()
+
