0a802fcf17091d1faebe23acf4b829cb80b49319
Fahd Arshad <fahdarshad123@gmail.com>
2017-04-13 11:55:56 +0500

    Update and rename alfred_enabled.list to seed.groovy

---
+++ seed/seed.groovy
@@ -0,0 +1,9 @@
+#!groovy
+
+// Clone Project, according to Gerrit Trigger
+//def repoUrl = "$GERRIT_SCHEME://$GERRIT_HOST:$GERRIT_PORT/$GERRIT_PROJECT"
+//def projectRoot = WORKSPACE
+
+// Reading through the ci_enablied.list
+def alfred_list = readFileFromWorkspace('alfred_enabled.list')
+String[] split_file = alfred_list.split(System.getProperty("line.separator"));
