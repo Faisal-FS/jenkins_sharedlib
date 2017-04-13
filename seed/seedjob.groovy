73ed934336aba79a1d52054cc23e5880bd4e7d7c
Fahd Arshad <fahdarshad123@gmail.com>
2017-04-13 14:12:29 +0500

    Update seedjob.groovy

--- seed/seedjob.groovy
+++ seed/seedjob.groovy
@@ -4,6 +4,7 @@
 //@Grab(group='org.apache.commons', module='commons-io', version='1.3.2')
 
 def sout = new StringBuilder(), serr= new StringBuilder()
+println $PROJECTURL
 
 // Clone Project, according to Gerrit Trigger
 def projectRoot = WORKSPACE + "/$PROJECT/"
