b2e0f4374d2adb59947ab60b14a3d6844a376bb5
Fahd Arshad <fahdarshad123@gmail.com>
2017-04-25 16:29:43 +0500

    Update template_seedjob.groovy

--- seed/template_seedjob.groovy
+++ seed/template_seedjob.groovy
@@ -1,28 +1,31 @@
 #!groovy
 
+// Including grape for config slurper
 @Grab(group='org.apache.commons', module='commons-io', version='1.3.2')
+
+// Cloning PROJECT env variable into WORKSPACE
 def sout = new StringBuilder(), serr= new StringBuilder()
-
-print "hello there muneeb"
-
 def projectRoot = WORKSPACE + "/$PROJECT/"
 def clone = "git clone $PROJECTURL".execute(null, new File(WORKSPACE + "/"))
 clone.consumeProcessOutput(sout, serr)
 clone.waitFor()
 println "out> $sout err> $serr"
 
+// Creating folders for each Repo on Alfred
 folder("$PROJECT")
 {
     displayName("$PROJECT")
-    description("pipeplines for $PROJECT")
+    description("Workloads for $PROJECT")
 }
 
+// Iterating over all configs in the directory
 new File("$projectRoot/alfred/pipelines").eachFile()
 {   file->
     println "Pipeline config:"
     println file.text
+
+    // Using configSlurper parsing contents of config file
     def config = new ConfigSlurper().parse(file.text)
-    
 
     // Creating workload based folders inside the repo folder
     folder("$PROJECT/$config.job.workload")
@@ -34,14 +37,16 @@ new File("$projectRoot/alfred/pipelines").eachFile()
     // Creating Pipeline
     pipelineJob("$PROJECT/$config.job.workload/$config.job.name")
     {
+        // Defines number of days to keep logs Default: 30 days
         logRotator(30,-1,-1,-1)
+
+        // Code chuck for periodic pipelines
         if( config.job.pipeline_type == "periodic" )
         {
             definition
             {
                 cpsScm
                 {
-                    label(config.job.label)
                     scm
                     {
                         git
@@ -54,21 +59,23 @@ new File("$projectRoot/alfred/pipelines").eachFile()
                             }
                         }
                     }
+                    // Adding configfile path to be used by the pipeline
                     scriptPath("alfred/pipelines/" + org.apache.commons.io.FilenameUtils.getBaseName(file.name))
                 }
             }
+
+            // Defining pipeline triggers
             triggers
             {
                 cron(config.job.schedule)
             }
+
+            // Post Build Cleanup
             publishers
             {
                 wsCleanup()
-                archiveArtifacts
-                {
-                    pattern(config.job.artifacts)
-                }
             }
         }
     }
 }
+
