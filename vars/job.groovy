3b9af91f97e93f97e46e0ec1d527df77efed03e8
Fahd Arshad <fahdarshad123@gmail.com>
2017-07-06 15:57:15 +0500

    Update job.groovy

--- vars/job.groovy
+++ vars/job.groovy
@@ -11,41 +11,54 @@ def call(body) {
   // Loading jenkinsLibrary
   def lib = new utils.JenkinsLibrary()
 
+  // Default timeout value
+  def timeValue = 24
+  if (args.timeout)
+  {
+    timeValue=args.timeout
+  }
+
   // Repo to clone for Alfred
   def repourl = "ssh://git@172.19.0.77:29418/source/${args.clone_repos}.git"
-timeout(time: 2, unit: 'NANOSECONDS') 
+
+  // Enforce timeout for pipelines that are stuck waiting for executors 
+  timeout(time: 2, unit: 'DAYS') 
   {
-  // Specifies the label which executes commands enclosed inside
-  node(args.label)
-  {
-    // Cleanup workspace at the start of the job
-    step([$class: 'WsCleanup'])
-
-    // Clone stage to fetch latest repository to be consumed by the pipeline
-    stage ('Clone')
+    // Specifies the label which executes commands enclosed inside
+    node(args.label)
     {
-        git credentialsId: 'jenkins', url: repourl
-    }
+      // Enforcing pipeline timeout 
+      timeout (time: timeValue, unit: 'HOURS')
+      { 
+        // Cleanup workspace at the start of the job
+        step([$class: 'WsCleanup'])
 
-    // Count of total stages found in jenkinsfile
-    def total_stages = lib.countStages(args)
+        // Clone stage to fetch latest repository to be consumed by the pipeline
+        stage ('Clone')
+        {
+            git credentialsId: 'jenkins', url: repourl
+        }
 
-    // Dynamically creating stages
-    for(int iter = 0; iter<total_stages; iter++)
-    {
-      try
-      {
-        lib.prepareStages(iter,args)
+        // Count of total stages found in jenkinsfile
+        def total_stages = lib.countStages(args)
+
+        // Dynamically creating stages
+        for(int iter = 0; iter<total_stages; iter++)
+        {
+          try
+          {
+            lib.prepareStages(iter,args)
+          }
+          catch (err)
+          {
+            lib.postPipeline(args,"FAILED")
+            error("Pipeline failed! Exiting ......")
+          }
+        }
+
+        // Post pipeline steps required for Alfred
+        lib.postPipeline(args,"PASSED")
       }
-      catch (err)
-      {
-        lib.postPipeline(args,"FAILED")
-        error("Pipeline failed! Exiting ......")
-      }
     }
-
-    // Post pipeline steps required for Alfred
-    lib.postPipeline(args,"PASSED")
-  }
   }
 }
